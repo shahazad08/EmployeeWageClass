6d88ad1d8efdc089ca51319c8e713d160c9b9365
shahazad08 <sk.shahazad@gmail.com>
2022-04-20 11:09:54 +0530

    Modified: If else is replaced with Swrich case

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -5,26 +5,26 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWageBuilder {
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME=1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
     public static void main(String args[])  {
         System.out.println("Welcome to Employee Wage Computation");
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME=1;
-        int EMP_RATE_PER_HOUR = 20;
+
         int empHours = 0;
         int empWage = 0;
        Random random = new Random();
         int empCheck = random.nextInt(2);
-        if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present");
-            empHours = 8;
-        }
-        else if(empCheck==IS_PART_TIME) {
-            System.out.println("Employee is Present, Half day");
-            empHours = 4;
-        }
-        else {
-            System.out.println("Employee is Absent");
-            empHours=0;
+        switch (empCheck){
+            case IS_FULL_TIME:
+                empHours = 8;
+                break;
+            case IS_PART_TIME:
+                empHours = 4;
+                break;
+            default:
+                empHours = 0;
         }
         empWage = empHours * EMP_RATE_PER_HOUR;
         System.out.println("The daily wages of an employee is : " + empWage);
