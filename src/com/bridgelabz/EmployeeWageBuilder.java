4ade39e3fc1bb0e2e757af8ddc0a4b02ad962d7e
shahazad08 <sk.shahazad@gmail.com>
2022-04-20 10:29:25 +0530

    Added: Calculate Daily Wage Functionality added

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -6,14 +6,21 @@ public class EmployeeWageBuilder {
     public static void main(String args[])  {
         System.out.println("Welcome to Employee Wage Computation");
         int IS_FULL_TIME = 1;
-        Random random = new Random();
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
+       Random random = new Random();
         int empCheck = random.nextInt(2);
         if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is Present");
+            empHours = 8;
         }
         else {
             System.out.println("Employee is Absent");
+            empHours=0;
         }
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.println("The daily wages of an employee is : " + empWage);
 
     }
 }
